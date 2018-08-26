56b78da08396396c377b3c7b7d357501c1e0bf2e
Jaiprashanth <jaitheking4858@gmail.com>
2018-08-26 18:54:22 +0800

    Updated the code with Send Mail Class

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Threading;
 
-namespace ConsoleApp1
+namespace MonitoringApp
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        static sendMail sm = new sendMail();
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Monitoring Service starting...");
+            //Create a Timer 
+            Timer timer = startMonitor();
+            //Wait for the user to hit <Enter>
+            Console.ReadLine();
+            Console.WriteLine("Please enter a key once");
+            timer.Dispose();
+
+
+
         }
+
+        public static Timer startMonitor()
+        {
+           Timer t= new Timer(TimerCallback, null, 0, 5000);
+            return t;
+        }
+
+        private static void TimerCallback(Object o)
+        {
+            Console.WriteLine(DateTime.Now + ": <Monitoring Start>");
+            sm.sendAlert("Test Alert","jaitheking4858@gmail.com")
+            Console.WriteLine("Email sent");
+
+            //Force a garbage collection to occur 
+            GC.Collect();
+        }
+
+
+        
+
+
     }
 }
---
+++ ConsoleApp1/ConsoleApp1/sendMail.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace MonitoringApp
+{
+    class sendMail
+    {
+
+        public bool sendAlert(string Message, string toWho)
+        {
+            bool sendSuccess = false;
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                mail.From = new MailAddress("jaitheking4858@gmail.com");
+                mail.To.Add(toWho);
+                mail.Subject = "Test Mail";
+                mail.Body = "This is for testing SMTP email from Gmail";
+                SmtpServer.Port = 587;
+                //Enter the credentials
+                SmtpServer.Credentials = new System.Net.NetworkCredential("email", "password");
+                SmtpServer.EnableSsl = true;
+                SmtpServer.Send(mail);
+                sendSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return sendSuccess;
+        }
+    }
+}
